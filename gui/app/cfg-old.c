3c1e61887088d9ae4cc7e3c61f706086af37f263
ib <ib@b3059339-0415-0410-9bf9-f77b7e298cf2>
2017-01-10 04:15:36 +0000

    Add missing #include.
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@37911 b3059339-0415-0410-9bf9-f77b7e298cf2

--- gui/app/cfg-old.c
+++ gui/app/cfg-old.c
@@ -21,6 +21,8 @@
  * @brief Translation of old settings or old configure options
  */
 
+#include <glib.h>
+
 /**
  * @brief Convert a filename into the original byte sequence
  *        according to the encoding specified in G_FILENAME_ENCODING,

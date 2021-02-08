5eb64a629e94768fd1dd78c985a937c1682f7557
reimar <reimar@b3059339-0415-0410-9bf9-f77b7e298cf2>
2021-02-08 22:02:18 +0000

    stream_cddb: update freedb server to gnudb.org.
    freedb.org is no longer available.
    
    
    git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@38251 b3059339-0415-0410-9bf9-f77b7e298cf2

--- stream/stream_cddb.c
+++ stream/stream_cddb.c
@@ -77,7 +77,7 @@
 #include "network.h"
 #include "libavutil/common.h"
 
-#define DEFAULT_FREEDB_SERVER "freedb.freedb.org"
+#define DEFAULT_FREEDB_SERVER "gnudb.gnudb.org"
 #define DEFAULT_CACHE_DIR     "/.cddb/"
 
 typedef struct {

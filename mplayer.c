sherpya/MPlayer

commit 53f6e8d316ba72021d54bcb28a1890331246b626

mplayer: uninit_player: Also set the ao pointer in the mixer to NULL
This avoids the problem addressed in the previous commit (r38414) by
making sure we don't call into AOs that were uninitialized.

Unfortunately it's not sufficient and causes a regression for AOs
where the call (even when the AO was already uninitialized) would
still work and unmute the audio (e.g. ao alsa).

So for example if a file is played with ao alsa and exits in muted
state. The mute will not be reversed and the alsa output device will
stay muted. If this is the user's main audio output device it would
usually mean the audio is completely muted for her system because
alsa is usually **the** low level audio device on her Linux system
used by everything else as well.

Patch by Reimar



git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@38415 b3059339-0415-0410-9bf9-f77b7e298cf2

mplayer.c
@@ -672,6 +672,7 @@ void uninit_player(unsigned int mask)
         if (mpctx->audio_out)
             mpctx->audio_out->uninit(mpctx->eof ? 0 : 1);
         mpctx->audio_out = NULL;
+        mpctx->mixer.audio_out = NULL;
     }
 
 #ifdef CONFIG_GUI

commit 3e3a78b62f27e96eeeb956135c96d84b6ea2d932

mplayer: Do not un-initialize components before exit_player_with_rc
After we finished all play tree items, we will call exit_player_with_rc.

When calling exit_player_with_rc, internally the routine will try
to uninit everything by itself.

More important: This way we unmute the AO if it's muted.

This avoids the regression introduced by the previous commit (r38415)
for typical usage, but it won't help for all potential cases. We still
fail to unmute correctly if one is using multiple AOs like this:

    mplayer file1 -ao pulse file2 -ao alsa file3 -ao pulse

It can even bite with one AO if there is a change to a file without
audio in as the last play tree item:

    ./mplayer file1 file2-without-audio-streams



git-svn-id: svn://svn.mplayerhq.hu/mplayer/trunk@38416 b3059339-0415-0410-9bf9-f77b7e298cf2

mplayer.c
@@ -4123,9 +4123,6 @@ goto_next_file:  // don't jump here after ao/vo/getch initialization!
                    (total_time_usage > 0.5) ? (total_frame_cnt / total_time_usage) : 0);
     }
 
-    // time to uninit all, except global stuff:
-    uninit_player(INITIALIZED_ALL - (INITIALIZED_GUI + INITIALIZED_INPUT + (fixed_vo ? INITIALIZED_VO : 0)));
-
     if (mpctx->eof == PT_NEXT_ENTRY || mpctx->eof == PT_PREV_ENTRY) {
         mpctx->eof = mpctx->eof == PT_NEXT_ENTRY ? 1 : -1;
         if (play_tree_iter_step(mpctx->playtree_iter, mpctx->play_tree_step, 0) == PLAY_TREE_ITER_ENTRY) {
@@ -4179,6 +4176,10 @@ goto_next_file:  // don't jump here after ao/vo/getch initialization!
         if (!mpctx->playtree_iter && !use_gui)
             filename = NULL;
         mpctx->eof = 0;
+
+        // time to uninit all, except global stuff:
+        uninit_player(INITIALIZED_ALL - (INITIALIZED_GUI + INITIALIZED_INPUT + (fixed_vo ? INITIALIZED_VO : 0)));
+
         goto play_next_file;
     }
 